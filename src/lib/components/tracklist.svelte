5321d7d6ae83df46cbe0e576f6a64e3351d886df
lorre huggan <lorre.huggan@protonmail.com>
2022-07-17 13:40:58 +0100

    add group hover to each track

--- src/lib/components/tracklist.svelte
+++ src/lib/components/tracklist.svelte
@@ -37,20 +37,20 @@
 	{#if selected.length > 0}
 		<ActiveSearch />
 		<h3 class="mb-8 text-2xl font-bold">Track List</h3>
-		{#each selected as data}
+		{#each selected.slice(0, 20) as data}
 			<div
-				class="mb-4 flex cursor-pointer items-center rounded p-2 transition-all duration-300 ease-in-out hover:bg-stone-800 "
+				class=" group relative mb-4 flex cursor-pointer items-center  overflow-hidden rounded p-2 transition-all delay-500 duration-300 ease-in-out hover:bg-gradient-to-r hover:from-c-teal-500 hover:to-c-pink-700"
 			>
 				<img
 					src={data.data.images[0].url}
 					alt={data.data.name}
-					class="mr-4 h-24 w-24 rounded shadow-main"
+					class="z-30 mr-4 h-24 w-24 rounded opacity-70 shadow-main transition-all duration-300 ease-in-out group-hover:-translate-y-2 group-hover:rotate-2 group-hover:opacity-100 "
 				/>
-				<div class="w-full">
+				<div class="z-30 w-full">
 					<div class="flex items-center">
 						<p class="text-lg font-black">
-							{data.data.name.length > 70
-								? `${data.data.name.substring(0, 70)}...`
+							{data.data.name.length > 50
+								? `${data.data.name.substring(0, 50)}...`
 								: data.data.name}
 						</p>
 						{#if data.data.explicit}
@@ -58,7 +58,7 @@
 							>
 						{/if}
 					</div>
-					<div class="">
+					<div class="z-30">
 						{#each data.data.artist as artist, i}
 							<a href={artist.external_urls.spotify}>
 								<span
@@ -70,12 +70,15 @@
 						{/each}
 					</div>
 				</div>
-				<div class="flex items-center">
+				<div class="z-30 flex items-center">
 					{#if data.data.preview_url}
 						<PreviewSong preview_url={data.data.preview_url} />
 					{/if}
 					<span class="text-sm">{convertMsToMinutesSeconds(data.data.duration)}</span>
 				</div>
+				<div
+					class=" absolute bottom-0 left-0 z-0 h-[90%] w-full translate-y-5 bg-black opacity-0 transition-all duration-500 ease-in-out group-hover:-translate-y-0 group-hover:opacity-100"
+				/>
 			</div>
 		{/each}
 	{:else if origin.length > 0}
